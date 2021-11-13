814eb59efc6bde6a902ba06a7fc7b6b340ceabe6
Aissa BEN YAHYA <aissabenbang@gmail.com>
2021-11-13 12:46:21 +0100

    client-server-chat

--- client.c
+++ client.c
@@ -19,7 +19,8 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256];
+    char client_message[256], server_message[256];
+
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
@@ -28,6 +29,7 @@ int main(int argc, char *argv[])
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
+    // get the IP using the name like (www.google.com)
     server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
@@ -39,19 +41,32 @@ int main(int argc, char *argv[])
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);
+
+    printf("[+] Connecting to the server...\n");
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    printf("[+] Connection Granted!\n");
+    while(1){
+
+        bzero(client_message,256);
+        bzero(server_message,256);
+
+        printf("Client: ");
+        fgets(client_message,255,stdin);
+        if(send(sockfd,client_message,strlen(client_message), 0) < 0)
+             error("ERROR sending to socket");
+
+        if(recv(sockfd,server_message,sizeof(server_message), 0) < 0) 
+             error("ERROR reading from socket");
+
+        printf("Server: %s\n",server_message);
+
+        if (strncmp("exit", server_message, 4) == 0) {
+            printf("Server Exit...\n");
+            break;
+        }
+    }
+
     close(sockfd);
     return 0;
 }
--- server.c
+++ server.c
@@ -17,58 +17,79 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr; // Two structure to hold server socketoptions and client socket option such as IP and Port
-     int n;
-   
-     // Check whether the port value is provided by the user or not
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-   
-     // create a server TCP socket
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-   
-     //clean the memory buffer of serv_addr
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-   
-     // Turn the privided port by user into an integer
-     portno = atoi(argv[1]); 
-     
-     // Set server IP and port
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno); // Turn the port from host format to network format
-   
-     // Bind the socket with IP and port
-     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-   
-     // Listen for client connection MAX 5 connections
-     printf("[+] Listening to incoming connections...\n")
-     listen(sockfd,5);
-   
-     clilen = sizeof(cli_addr);
-   
-     // Accept an incoming connection from the client
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0) // check whether the socket is opened or not 
-          error("ERROR on accept");
-     print("[+] Connection accepted...\n")
-     // Clean the buffer
-     bzero(buffer,256);
-   
-     if(recv(newsockfd,buffer,sizeof(buffer), 0);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char client_message[256];
+    char server_message[256];
+    struct sockaddr_in serv_addr, cli_addr; // Two structure to hold server socketoptions and client socket option such as IP and Port
+    int n;
+
+    // Check whether the port value is provided by the user or not
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+
+    // create a server TCP socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+       error("ERROR opening socket");
+
+    //clean the memory buffer of serv_addr
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    // Turn the privided port by user into an integer
+    portno = atoi(argv[1]); 
+    
+    // Set server IP and port
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno); // Turn the port from host format to network format
+
+    // Bind the socket with IP and port
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+             error("ERROR on binding");
+
+    // Listen for client connection MAX 5 connections
+    printf("[+] Listening to incoming connections...\n");
+    listen(sockfd,5);
+
+    clilen = sizeof(cli_addr);
+
+    // Accept an incoming connection from the client
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) // check whether the socket is opened or not 
+         error("ERROR on accept");
+         
+    printf("[+] Connection accepted...\n");
+    printf("==================== Starting the chat ===================\n");
+
+    while(1){
+        // Clean the buffers
+        bzero(client_message,256);
+        bzero(server_message,256);
+
+        // Reveive message from the client    
+        if(recv(newsockfd,client_message,sizeof(client_message), 0) < 0)
+           error("ERROR reading from socket");
+
+        printf("Client: %s\n",client_message);
+
+        printf("Server: ");
+        fgets(server_message, 256, stdin);
+
+        // send server message to the client
+        if(send(newsockfd,server_message,strlen(server_message), 0) < 0)
+            error("ERROR writing to socket");
+        
+        if (strncmp("exit", client_message, 4) == 0) {
+            printf("Server Exit...\n");
+            break;
+        }
+    }
+
+    // close both client and server sockets
+    close(newsockfd);
+    close(sockfd);
+    return 0; 
 }
