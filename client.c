46d0658edda2ec135dbb59d057dc819487657d24
Aissa-benyahya <77437740+Aissa-benyahya@users.noreply.github.com>
2021-11-14 20:38:28 +0100

    Added windows compatibility

--- client.c
+++ client.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
+
+#ifdef LINUX_KEY_WORD
+    #include <unistd.h>
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+#elif WINDOWS_KEY_WORD
+    #include <WinSock2.h>
 
 void error(const char *msg)
 {
