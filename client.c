7e0d93b08b3a7355ed5b3ce78aa8b8030a148e72
Aissa-benyahya <77437740+Aissa-benyahya@users.noreply.github.com>
2021-11-14 23:46:14 +0100

    fix indentation

--- client.c
+++ client.c
@@ -21,89 +21,91 @@ void error(const char *msg)
 int main(int argc, char *argv[])
 {
 	#ifdef __linux__
-    	int sockfd, portno;
-    	struct sockaddr_in serv_addr; // Two structure to hold server socketoptions and client socket option such as IP and Port
-    #elif __WIN32
-    	WSADATA wsaData;
-    	SOCKET sockfd;
-    	SOCKADDR_IN serv_addr;
-    	int portno;
+    		int sockfd, portno;
+    		struct sockaddr_in serv_addr; // Two structure to hold server socketoptions and client socket option such as IP and Port
+    	#elif __WIN32
+    		WSADATA wsaData;
+    		SOCKET sockfd;
+    		SOCKADDR_IN serv_addr;
+    		int portno;
 
 		if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
 		{
-        	printf("Server: WSAStartup failed with error %ld\n", WSAGetLastError());
-        	// Exit with error
+        		printf("Server: WSAStartup failed with error %ld\n", WSAGetLastError());
+        		// Exit with error
 			return -1;
-    	}
-    	else
+    		}
+    		else
 		{
-        	printf("Server: The Winsock dll found!\n");
-       	 	printf("Server: The current status is %s.\n", wsaData.szSystemStatus);
+        		printf("Server: The Winsock dll found!\n");
+       	 		printf("Server: The current status is %s.\n", wsaData.szSystemStatus);
+    		}
+    	#else
+	
+    	#endif
+	
+    	struct hostent *server;
+
+    	char client_message[256], server_message[256];
+
+    	if (argc < 3) {
+       		fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       		exit(0);
     	}
-    #else
-    #endif
-    struct hostent *server;
-
-    char client_message[256], server_message[256];
-
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
+    	portno = atoi(argv[2]);
+    	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    	if (sockfd < 0) 
+        	error("ERROR opening socket");
     // get the IP using the name like (www.google.com)
-    #ifdef __linux__
-    	server = gethostbyname(argv[1]);
-    	if (server == NULL) {
-        	fprintf(stderr,"ERROR, no such host\n");
-        	exit(0);
-    	}
+    	#ifdef __linux__
+    		server = gethostbyname(argv[1]);
+    		if (server == NULL) {
+        		fprintf(stderr,"ERROR, no such host\n");
+        		exit(0);
+    		}
 	#endif
 		
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    #ifdef __WIN32
-    	if (isalpha(argv[1][0]))
-    	{   // server address is a name
-        	server = gethostbyname(argv[1]);
+    	memset(&serv_addr, 0, sizeof(serv_addr));
+    	serv_addr.sin_family = AF_INET;
+    	#ifdef __WIN32
+    		if (isalpha(argv[1][0]))
+    		{   // server address is a name
+        		server = gethostbyname(argv[1]);
+    		}
+    		else
+    		{ // Convert nnn.nnn address to a usable one
+        		int addr = inet_addr(argv[1]);
+        		server = gethostbyaddr((char *)&addr, 4, AF_INET);
+    		}
+    	#endif
+    	memcpy(&(serv_addr.sin_addr), server->h_addr, server->h_length);
+    	serv_addr.sin_port = htons(portno);
+
+    	printf("[+] Connecting to the server...\n");
+    	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        	error("ERROR connecting");
+    	printf("[+] Connection Granted!\n");
+    	while(1){
+
+        	memset(client_message,0, strlen(client_message));
+        	memset(server_message,0, strlen(server_message));
+
+        	printf("Client: ");
+        	fgets(client_message,255,stdin);
+        	if(send(sockfd,client_message,strlen(client_message), 0) < 0)
+             	error("ERROR sending to socket");
+
+        	if(recv(sockfd,server_message,sizeof(server_message), 0) < 0) 
+             	error("ERROR reading from socket");
+
+        	printf("Server: %s\n",server_message);
+
+       	 	if (strncmp("exit", server_message, 4) == 0) {
+            	printf("Server Exit...\n");
+           	 break;
+        	}
     	}
-    	else
-    	{ // Convert nnn.nnn address to a usable one
-        	int addr = inet_addr(argv[1]);
-        	server = gethostbyaddr((char *)&addr, 4, AF_INET);
-    	}
-    #endif
-    memcpy(&(serv_addr.sin_addr), server->h_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
 
-    printf("[+] Connecting to the server...\n");
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("[+] Connection Granted!\n");
-    while(1){
-
-        memset(client_message,0, strlen(client_message));
-        memset(server_message,0, strlen(server_message));
-
-        printf("Client: ");
-        fgets(client_message,255,stdin);
-        if(send(sockfd,client_message,strlen(client_message), 0) < 0)
-             error("ERROR sending to socket");
-
-        if(recv(sockfd,server_message,sizeof(server_message), 0) < 0) 
-             error("ERROR reading from socket");
-
-        printf("Server: %s\n",server_message);
-
-        if (strncmp("exit", server_message, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
-    }
-
-    close(sockfd);
-    return 0;
+    	close(sockfd);
+    	return 0;
 }
